b6d44a0986b8979103848cf78244f9dd8da3c0a0
VictorE14 <tuzdzidzv@gmail.com>
2026-05-24 16:52:48 -0500

    Delete src directory

--- src/components/TramiteCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function TramiteCard({ title, desc, icon, onClick }) {
-  return (
-    <div className="tramite-card" onClick={onClick}>
-      <div className="tramite-icon">{icon}</div>
-      <div className="tramite-title">{title}</div>
-      <div className="tramite-desc">{desc}</div>
-    </div>
-  )
-}
